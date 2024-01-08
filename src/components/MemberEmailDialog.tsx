d0f5f72135a8f8d4a4f3f13a7c715799ac91c86d
Xinghan Yang <yang-xinghan@outlook.com>
2024-01-08 13:36:33 -0800

    Update MemberEmailDialog.tsx

--- src/components/MemberEmailDialog.tsx
+++ src/components/MemberEmailDialog.tsx
@@ -30,18 +30,9 @@ const MemberEmailDialog: React.FC<MemberEmailDialogProps> = ({
 
         const today = Date.now();
 
-        let emails;
-
-        switch (copyEmailOption) {
-            case "copySelected":
-                emails = memberRowSelectionModel
-                    .map((id) => retrieveMemberById(id.toString())?.email)
-                    .filter(Boolean) as string[];
-                break;
-
-
-        }
-
+        const emails=memberRowSelectionModel
+            .map((id) => retrieveMemberById(id.toString())?.email)
+            .filter(Boolean) as string[];
         setEmails(emails);
     }, [open, copyEmailOption, memberRowSelectionModel, retrieveMemberById, membersData]);
 
