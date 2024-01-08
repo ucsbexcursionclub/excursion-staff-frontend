cfce8f84bcb668ec5d908186356c12a60625d486
Xinghan Yang <yang-xinghan@outlook.com>
2024-01-08 13:29:30 -0800

    Revert "Update"
    This reverts commit 0584b0869e16a2f58ec5eb121bbc02f7e52be47f.

--- src/components/MemberEmailDialog.tsx
+++ src/components/MemberEmailDialog.tsx
@@ -31,13 +31,18 @@ const MemberEmailDialog: React.FC<MemberEmailDialogProps> = ({
         const today = Date.now();
 
         let emails;
-        // eslint-disable-next-line prefer-const
-        emails = memberRowSelectionModel
-                .map((id) => retrieveMemberById(id.toString())?.email)
-                .filter(Boolean) as string[];
+
+        switch (copyEmailOption) {
+            case "copySelected":
+                emails = memberRowSelectionModel
+                    .map((id) => retrieveMemberById(id.toString())?.email)
+                    .filter(Boolean) as string[];
+                break;
+
+
+        }
+
         setEmails(emails);
-
-
     }, [open, copyEmailOption, memberRowSelectionModel, retrieveMemberById, membersData]);
 
     const maxWidth = `${(screenwidth / 100) * 80}vw`;
