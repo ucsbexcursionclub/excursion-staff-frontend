042f8cfc2a8a633d3425b1e297e939efa34dd426
mariekarpinska <karpinska@umail.ucsb.edu>
2024-11-07 20:08:54 -0800

    use Avatar not img

--- src/components/StaffCard.tsx
+++ src/components/StaffCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {StaffProfile} from "../utils/types";
 import {capitalizeFirstLetter, generateResourceUrl} from "../utils/utils";
+import {Avatar} from "@mui/material";
 
 interface StaffCardProps {
     staff: StaffProfile;
@@ -10,13 +11,13 @@ const StaffCard: React.FC<StaffCardProps> = ({staff}) => {
     return (
         <div className="w-full h-full p-2">
             <div className="flex flex-col items-center w-full bg-gray-100 rounded-2xl shadow-sm p-4">
-                <img
+                <Avatar
                     src={generateResourceUrl(staff.profileImagePath || "/resources/avatar.png")}
                     alt={staff.name}
-                    className="w-full h-full sm:w-40 sm:h-40 md:w-52 md:h-52 object-cover rounded-md my-4"
+                    className="w-full h-full sm:mx-1 sm:mx-2 md:mx-3 lg:mx-6 object-cover rounded-md my-2"
                 />
                 <div className="w-full text-center">
-                    <h3 className="text-lg sm:text-xl md:text-2xl font-bold text-black mb-2">
+                    <h3 className="text-lg sm:text-xl md:text-2xl font-bold text-black">
                         {capitalizeFirstLetter(staff.name)}
                     </h3>
                     <p className="text-sm sm:text-base md:text-lg text-gray-700 mb-1">
